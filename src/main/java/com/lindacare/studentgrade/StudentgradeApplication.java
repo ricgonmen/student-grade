33e6b95dabb7dbe3bded06626593a2fb791d26a8
Richard <ricgomen@gmail.com>
2018-10-28 21:45:47 +0100

    Recien creado

---
+++ src/main/java/com/lindacare/studentgrade/StudentgradeApplication.java
@@ -0,0 +1,12 @@
+package com.lindacare.studentgrade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentgradeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentgradeApplication.class, args);
+	}
+}
---
+++ src/test/java/com/lindacare/studentgrade/StudentgradeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.lindacare.studentgrade;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StudentgradeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
